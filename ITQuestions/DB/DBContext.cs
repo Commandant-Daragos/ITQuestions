a452d61e84c6ad05cebfe1ee1b61dc58ca42d4ae
Dominik Medvecký <dominikmedvecky@gmail.com>
2025-08-08 22:44:24 +0200

    Add project files.

---
+++ ITQuestions/DB/DBContext.cs
@@ -0,0 +1,21 @@
+﻿using ITQuestions.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITQuestions.DB
+{
+    public class DBContext : DbContext
+    {
+        public DbSet<Question> Questions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Placeholder for future connection string
+            optionsBuilder.UseSqlServer("DB connection string"); // or some other database, will see
+        }
+    }
+}
